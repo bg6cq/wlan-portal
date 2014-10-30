83561153a112b2e57a97d949f798ae3c119980b4
Zhang Huanjie <james@ustc.edu.cn>
2014-10-30 18:17:42 +0800

    change pass sms

--- ip.c
+++ ip.c
@@ -345,7 +345,7 @@ void Stage1() // sendsms, dispay input page
 		strncpy(pass,row[0],7);
 	snprintf(buf,MAXLEN,"replace into PhonePass values ('%s', '%s', date_add(now(), interval 8 day) )",PHONE,pass);
 	ExecSQL(buf,0);
-	snprintf(buf,MAXLEN,"php /usr/src/sendsms/sendsms.php %s \"您在中国科大访客WLAN的密码是%s，一周内都可以使用本密码登录，请保留本短信。\" 2>/dev/null",PHONE,pass);
+	snprintf(buf,MAXLEN,"php /usr/src/sendsms/sendsms.php %s \"%s是您在中国科大访客WLAN密码，一周内都可以使用本密码登录，请保留本短信。\" 2>/dev/null",PHONE,pass);
 	fp=popen(buf,"r");
 	if(fp==NULL){
 		snprintf(buf,MAXLEN,"insert into Log values('%s','%s',now(),'send pass to %s error')",
